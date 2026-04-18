ac68cb3872c3f397c43f5594e921674fea82e710
Jyothirmayi <225334938+Jyothirmayi13@users.noreply.github.com>
2026-04-18 16:52:27 +0530

    Update avl.c

--- avl.c
+++ avl.c
@@ -1,2 +1,15 @@
-
-void RR(NODE ROOT){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "avl.h"
+AVLNODE create(Appointment appt) {
+    AVLNODE node = (AVLNODE)malloc(sizeof(AVLNODE));
+    if (node == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    node->data   = appt;
+    node->ht = 1;
+    node->left = node->right = NULL; 
+    return node;
+}
