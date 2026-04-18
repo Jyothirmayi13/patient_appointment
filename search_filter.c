7e8e0310e4b0cb03c30607c4fd40255792a86c68
parnika-pothula <pothulaparnika@gmail.com>
2026-04-18 22:43:26 +0530

    Update search_filter.c

--- search_filter.c
+++ search_filter.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+#include <patient_history.c>
+typedef struct Node {
+int id;
+char name[50];
+int age;
+char slot[20];
+struct Node *left, *right;
+} Node;
+Node ID(Node* root,int id){
+  
+}
