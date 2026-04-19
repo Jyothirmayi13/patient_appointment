c91bd399b7817723fda503122b5b9d9b7fdb1c54
Jyothirmayi <225334938+Jyothirmayi13@users.noreply.github.com>
2026-04-19 09:34:19 +0530

    Update avl.h

--- avl.h
+++ avl.h
@@ -1,1 +1,15 @@
-
+typedef struct Appointment {
+    int id;                 
+    char patientName[50];
+    char docName[50];
+    char date[15];           
+    char timeSlot[10];       
+    int priority;           
+} Appointment;
+struct node {
+    Appointment data;
+    int ht;
+    struct node* left;
+    struct node* right;
+};
+typedef struct node* AVLNODE;
