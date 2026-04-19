982f420eccc50441db7097bba01ceb4f60aec10c
Jyothirmayi <225334938+Jyothirmayi13@users.noreply.github.com>
2026-04-19 09:30:28 +0530

    Update avl.c

--- avl.c
+++ avl.c
@@ -13,3 +13,56 @@ AVLNODE create(Appointment appt) {
     node->left = node->right = NULL; 
     return node;
 }
+int height(AVLNODE root) {
+    int lh, rh;
+    if(root == NULL){
+        return 0;
+    }
+    if(root->left == NULL)
+        lh = 0;
+    else
+        lh = 1 + root->left->ht;
+    if(root->right == NULL)
+        rh = 0;
+    else
+        rh = 1 + root->right->ht;
+    if(lh > rh)
+        return lh;
+    else
+        return rh;
+}
+int balancefactor(AVLNODE root) {
+    int lh, rh;
+    if(root == NULL)
+        return 0;
+    if(root->left == NULL)
+        lh = 0;
+    else
+        lh = 1 + root->left->ht;
+    if(root->right == NULL)
+        rh = 0;
+    else
+        rh = 1 + root->right->ht;
+    return (lh - rh);
+}
+
+
+AVLNODE rotateRight(AVLNODE x) {
+    AVLNODE y;
+    y = x->left;
+    x->left = y->right;
+    y->right = x;
+    x->ht = height(x);
+    y->ht = height(y);
+    return y;
+}
+
+AVLNODE rotateLeft(AVLNODE x) {
+    AVLNODE y;
+    y = x->right;
+    x->right = y->left;
+    y->left = x;
+    x->ht = height(x);
+    y->ht = height(y);
+    return y;
+}
