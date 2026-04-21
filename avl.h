b9d0501e3199744bc84cde160d30dad4a994f358
Jyothirmayi <225334938+Jyothirmayi13@users.noreply.github.com>
2026-04-21 19:40:10 +0530

    Update avl.h

--- avl.h
+++ avl.h
@@ -4,14 +4,25 @@ typedef struct Appointment {
     char docName[50];
     char date[15];           
     char timeSlot[10];       
-    int priority;           
+    int priority;
+struct Queue history;
 } Appointment;
 struct node {
     Appointment data;
     int ht;
     struct node* left;
     struct node* right;
+struct Queue history; 
 };
+typedef struct QNode {
+    Appointment data;
+    struct QNode* next;
+} QNode;
+typedef struct Queue {
+    QNode* front;
+    QNode* rear;
+} Queue;
+
 typedef struct node* AVLNODE;
 AVLNODE create(Appointment appt);
 int height(AVLNODE root);
@@ -21,3 +32,4 @@ AVLNODE rotateRight(AVLNODE x);
 AVLNODE insertNodeInAVL(AVLNODE root, Appointment appt);
 AVLNODE deleteNodeInAVL(AVLNODE root, int id);
 AVLNODE search(AVLNODE root, int id);
+void addToHistory(AVLNODE node, Appointment appt);
