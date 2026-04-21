ed27f7208b01d16fa33acbfa1cae0fc75d1eecfe
Jyothirmayi <225334938+Jyothirmayi13@users.noreply.github.com>
2026-04-21 19:46:17 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,1 +1,26 @@
 
+void addToHistory(AVLNODE node, Appointment appt)
+{
+    if (node == NULL)
+        return;
+
+    QNode* temp = (QNode*)malloc(sizeof(QNode));
+
+    if (temp == NULL){
+        printf("Memory allocation failed\n");
+        return;}
+
+    temp->data = appt;
+    temp->next = NULL;
+    if (node->history.rear == NULL)
+     node->history.front = node->history.rear = temp;
+    else
+    {
+        node->history.rear->next = temp;
+        node->history.rear = temp;
+    }
+
+    printf("Appointment added to history\n");
+}
+void patientdetails(Queue *q)
+//traverse queue!
